f83e3a6cc38ee6ab748b6505ccb2dc644adeb3f4
VIIZET <129474307+viizet@users.noreply.github.com>
2023-12-29 22:16:03 +0300

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -21,7 +21,7 @@ AUTH = config("AUTH", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
-userbot = Client("saverestricted", session_string=SESSION, api_hash=API_HASH, api_id=API_ID) 
+userbot = Client("saverallwaxwalbobot", session_string=SESSION, api_hash=API_HASH, api_id=API_ID) 
 
 try:
     userbot.start()
@@ -30,7 +30,7 @@ except BaseException:
     sys.exit(1)
 
 Bot = Client(
-    "SaveRestricted",
+    "saverallwaxwalbobot",
     bot_token=BOT_TOKEN,
     api_id=int(API_ID),
     api_hash=API_HASH
